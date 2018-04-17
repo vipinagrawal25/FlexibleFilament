96570a4a5c0be7334acdba5b41191cb4a7729a8f
dhrubaditya <dhruba.mitra@gmail.com>
2018-04-17 13:53:15 +0200

    Added an example of input.h file

---
+++ input.h
@@ -0,0 +1,8 @@
+#ifndef FILE_INPUT_SEEN
+#define FILE_INPUT_SEEN
+/* ----------------------------------------*/
+unsigned int const Nensemble=1;
+double TMAX=10.,dt=0.006;
+int idiag=1;
+/* ----------------------------------------*/
+#endif /* !FILE_INPUT_SEEN */
