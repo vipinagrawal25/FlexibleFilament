1f451706d2f6a828e55f2568c6b77b2ab3d16856
Vipin Agrawal <vipin@nordita220lm.nordita.su.se>
2018-05-28 19:08:03 +0200

    Add one more line for rnkf45

--- src/ode.h
+++ src/ode.h
@@ -2,3 +2,4 @@ void eval_rhs(double time,double y[],double rhs[]);
 void euler(unsigned int ndim, double *y, double time,double dt);
 void rnkt2(unsigned int ndim, double *y, double time,double dt);
 void rnkt4(unsigned int ndim, double *y, double time,double dt);
+void rnkf45(unsigned int ndim, double *y, double time, double *dt);
