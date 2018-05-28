25238c83f168783b5b4154d76982ffcf5e01e5f6
Vipin Agrawal <vipin@nordita220lm.nordita.su.se>
2018-05-28 19:16:14 +0200

    Made changes to write in a file and to play with rnkf45

--- src/solve.cpp
+++ src/solve.cpp
@@ -3,11 +3,13 @@
 #include<stdlib.h>
 #include<time.h>
 #include<math.h>
+#include<string>
 #include "ode.h"
 #include "model.h"
 #include "input.h"
 //#include "cuda.h"
 using namespace std;
+void diagnos(int p);
 /**************************/
 unsigned int const ndim=Nensemble*pdim;
 /* ----------------------------------------*/
@@ -17,33 +19,63 @@ int main(){
   int k;
   int ldiagnos=0;
   iniconf(y);
+  // cout << HH << endl;
   //  setup_cuda();
 //----------------------------
-  int itn=0;
-  ofstream outfile;
-  outfile.open("position.txt", ios::out); 
-  while(time <= TMAX){
+  int itn=1;  
+  // int temp = log10(TMAX/dt);
+  // int temp2 = pow(10,temp+1);
+  
+  // Deleting contents of the folder and creating folder again.
+  system("exec rm -fr output");    
+  system("exec mkdir output");
+  ofstream outfile_time;
+  outfile_time.open("output/time.txt", ios::out); 
 
-    /* Changes made by Vipin to write in a file*/
+  while(time < TMAX){
 
-    outfile << time << '\t' << y[0] << '\t' << y[1] << '\t' << y[2] << '\t' << y[3] << endl;
-
-    //    printf("%f\t%f\t%f\n",time,y[0],y[2]);
-	//    if (ldiagnos==0) {Diagnostics(itn,&y[0],time);};
     for(int ibody=0;ibody<Nensemble;ibody++){
+      time = time+dt;
       int irb=pdim*ibody;
-      //euler(pdim,&y[irb],time,dt);
-      //rnkt2(pdim,&y[irb],time,dt);
-      rnkt4(pdim,&y[irb],time,dt);
+      //euler(pdim,&y[irb],time-dt,dt);
+      //rnkt2(pdim,&y[irb],time-dt,dt);
+      // rnkt4(pdim,&y[irb],time-dt,dt);
+
+      rnkf45(pdim, &y[irb], time-dt, &dt);
+      // cout << dt << endl;
     }
-    time=time+dt;
-    itn=itn+1;
     ldiagnos=itn%idiag;
-  } 
-  //cout<<"Done, time="<<time-dt<<"\t TMAX="<<TMAX<<"\n";
+  if (ldiagnos == 0) {
+    outfile_time << time << '\t' ;
+     // cout << time << '\t' << y[0] << '\t' << (sin(2*time+10*sin(0.1*time)))/sqrt(6+3*cos(0.1*time)) << '\t' << 1/sqrt(6+3 *cos(0.1*time))<<endl;
+      // cout << dt << endl;
+    ofstream outfile;
+    string l = "output/position";
+    l.append(to_string(itn/idiag));
+    l.append(".txt");
+    outfile.open(l, ios::out);
 
-  outfile.close();
+    for (int ip = 0; ip < Np; ++ip)
+      {
+        outfile << y[3*ip] << '\t' << y[3*ip+1] << '\t' << y[3*ip+2] << endl ;
+      }
 
+    outfile.close();
+
+    // for (int ip = 0; ip < Np; ++ip)
+    // {
+    //   outfile << y[3*ip+2] << '\t';
+    // }
+    // outfile << endl;
+  }
+
+  cout<<"Done, time="<<time<<"\t TMAX="<<TMAX<<"\n";
+  itn=itn+1;
+
+
+}
+outfile_time.close();
+cout << itn << endl;
 //----------------------------
 }
 /* ----------------------------------------*/
