54d5c1b705c07b3bab74db97db81abd17d100592
Vipin Agrawal <Vipin Agrawal>
2018-07-20 13:34:02 +0530

    decleared the functions

--- src/ode.h
+++ src/ode.h
@@ -2,5 +2,5 @@ void eval_rhs(double time,double y[],double rhs[], bool flag_kappa, double CurvS
 // void euler(unsigned int ndim, double *y, double time,double dt);
 // void rnkt2(unsigned int ndim, double *y, double time,double dt);
 // void rnkt4(unsigned int ndim, double *y, double time,double dt);
-void rnkf45(unsigned int ndim, double *y, double time, double *dt, double* CurvSqr, double* SS, double ldiagnos);
-void DP54(unsigned int ndim, double *y, double time, double *dt, double* CurvSqr, double* SS, double ldiagnos);
+void rnkf45(unsigned int ndim, double *y, double *time, double *dt, double* CurvSqr, double* SS, double ldiagnos);
+void DP54(unsigned int ndim, double *y, double *time, double *dt, double* CurvSqr, double* SS, double ldiagnos);
