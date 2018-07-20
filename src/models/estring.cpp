13ac336696d438bfcbb5ab8a949316ccc2dae718
Vipin Agrawal <Vipin Agrawal>
2018-07-20 13:24:21 +0530

    Updated a bit

--- src/models/estring.cpp
+++ src/models/estring.cpp
@@ -31,7 +31,7 @@ void eval_rhs(double time,double y[],double rhs[], bool flag_kappa, double CurvS
   }
   
   // External force applied on the end point.
-  vec3 FF0(0., 0., -FFZ0*sin(omega*time));
+  vec3 FF0(0., 0., FFZ0);
   // cout << FF0.z <<endl;
   dR[Np-1] = dR[Np-1]-FF0*1/Gamma; 
   //dR[Np-1].y = 0;                     // Constraint that last point should always remain on z axis. 
@@ -59,107 +59,209 @@ void dHdR(int kp, vec3 X[], vec3* add_FF, double* add_kappasqr, double* add_SS, 
   //vec3 FF;
   Xzero.x=0.; Xzero.y=0.;Xzero.z=Z0;
 
-  if (kp == 0)
-  {
-    dX = X[kp-1+1]-Xzero;
-    bkm1 = norm(dX);
-    ukm1=dX/bkm1;
-    getub(&bk, &uk, kp, X);
-    getub(&bkp1, &ukp1, kp+1, X);
-    FF = (     (uk)/bkm1 - (ukm1+ukp1)/bk
-         + (uk/bk)*( dot(uk,ukm1) + dot(uk,ukp1) )
-         - (ukm1/bkm1)*( dot(ukm1,uk) )
-         );
-    FF = FF*AA/aa;
-    // Add an extra term for inextensibility constraint
-    FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa))*HH/aa; 
-    // cout << FF.z << endl;
-    *add_kappasqr=0.;
-    *add_FF = FF;
-  }
+  switch(kp){
+    case 0:
+      dX = X[kp-1+1]-Xzero;
+      bkm1 = norm(dX);
+      ukm1=dX/bkm1;
+      getub(&bk, &uk, kp, X);
+      getub(&bkp1, &ukp1, kp+1, X);
+      FF = (     (uk)/bkm1 - (ukm1+ukp1)/bk
+           + (uk/bk)*( dot(uk,ukm1) + dot(uk,ukp1) )
+           - (ukm1/bkm1)*( dot(ukm1,uk) )
+           );
+      FF = FF*AA/aa;
+      // Add an extra term for inextensibility constraint
+      FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa))*HH/aa; 
+      // cout << FF.z << endl;
+      *add_kappasqr=0.;
+      *add_FF = FF;
+      *add_SS = (kp+1)*bkm1;
+      break;
 
-  else if (kp == 1)
-  {
-    dX = X[kp-2+1]-Xzero;
-    bkm2 = norm(dX);
-    ukm2 = dX/bkm2;
-    getub(&bkm1, &ukm1, kp-1, X);
-    getub(&bk, &uk, kp, X);
-    getub(&bkp1, &ukp1, kp+1, X);
-    FF = (     (uk+ukm2)/bkm1 - (ukm1+ukp1)/bk
-        + (uk/bk)*( dot(uk,ukm1) + dot(uk,ukp1) )
-        - (ukm1/bkm1)*( dot(ukm1,ukm2) + dot(ukm1,uk) )
-        );
-    FF = FF*(AA/aa);
-    // cout << FF.z << endl;
-    FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa) )*HH/aa;   // Inextensibility constraint
-    *add_kappasqr=0.;
-    *add_FF = FF;
-  }
+    case 1:
+      dX = X[kp-2+1]-Xzero;
+      bkm2 = norm(dX);
+      ukm2 = dX/bkm2;
+      getub(&bkm1, &ukm1, kp-1, X);
+      getub(&bk, &uk, kp, X);
+      getub(&bkp1, &ukp1, kp+1, X);
+      FF = (     (uk+ukm2)/bkm1 - (ukm1+ukp1)/bk
+          + (uk/bk)*( dot(uk,ukm1) + dot(uk,ukp1) )
+          - (ukm1/bkm1)*( dot(ukm1,ukm2) + dot(ukm1,uk) )
+          );
+      FF = FF*(AA/aa);
+      // cout << FF.z << endl;
+      FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa) )*HH/aa;   // Inextensibility constraint
+      *add_kappasqr=0.;
+      *add_FF = FF;
+      *add_SS = (kp+1)*bkm1;   
+      break;
 
+    case Np-2:
+      getub(&bkm2, &ukm2, kp-2, X);
+      getub(&bkm1, &ukm1, kp-1, X);
+      getub(&bk, &uk, kp, X);
 
-  else if (kp == Np-2)
-  {
-    getub(&bkm2, &ukm2, kp-2, X);
-    getub(&bkm1, &ukm1, kp-1, X);
-    getub(&bk, &uk, kp, X);
+      FF = (     (uk+ukm2)/bkm1 - (ukm1)/bk
+          + (uk/bk)*( dot(uk,ukm1))
+          - (ukm1/bkm1)*( dot(ukm1,ukm2) + dot(ukm1,uk) )
+          );    
+      FF = FF*(AA/aa);
+      // cout << bk << endl;
+      // cout << FF.z << endl;
+      FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa))*HH/aa;    // Inextensibility constraint 
+      // cout << FF.z << endl;
+      *add_kappasqr=0.;
+      *add_FF = FF;  
+      *add_SS = (kp+1)*bkm1;
+      break;
 
-    FF = (     (uk+ukm2)/bkm1 - (ukm1)/bk
-        + (uk/bk)*( dot(uk,ukm1))
-        - (ukm1/bkm1)*( dot(ukm1,ukm2) + dot(ukm1,uk) )
-        );    
-    FF = FF*(AA/aa);
-    // cout << bk << endl;
-    // cout << FF.z << endl;
-    FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa))*HH/aa;    // Inextensibility constraint 
-    // cout << FF.z << endl;
-    *add_kappasqr=0.;
-    *add_FF = FF;
-  }
-
-  else if (kp == Np-1)
-  {
-    getub(&bkm2, &ukm2, kp-2, X);
-    getub(&bkm1, &ukm1, kp-1, X);
+    case Np-1:
+      getub(&bkm2, &ukm2, kp-2, X);
+      getub(&bkm1, &ukm1, kp-1, X);
   
-    FF = (     (ukm2)/bkm1
+      FF = (     (ukm2)/bkm1
         - (ukm1/bkm1)*( dot(ukm1,ukm2) )
         );
-    FF = FF*(AA/aa);
-    // cout << bkm1 << endl;
-    FF = FF - (ukm1*(bkm1-aa))*HH/aa;
-    *add_kappasqr=0.;
-    *add_FF = FF;
+      FF = FF*(AA/aa);
+      // cout << bkm1 << endl;
+      // cout << FF.y << endl;
+      FF = FF - (ukm1*(bkm1-aa))*HH/aa;
+      // cout << FF.y << endl;
+      *add_kappasqr=0.;
+      *add_FF = FF;
+      *add_SS = (kp+1)*bkm1;
+      break;
+
+    default:
+      getub(&bkm2, &ukm2, kp-2, X);
+      getub(&bkm1, &ukm1, kp-1, X);
+      getub(&bk, &uk, kp, X);
+      getub(&bkp1, &ukp1, kp+1, X);
+      FF = (     (uk+ukm2)/bkm1 - (ukm1+ukp1)/bk
+          + (uk/bk)*( dot(uk,ukm1) + dot(uk,ukp1) )
+          - (ukm1/bkm1)*( dot(ukm1,ukm2) + dot(ukm1,uk) )
+          );
+      FF = FF*(AA/aa);
+      // cout << bkm1 <<endl;
+      // cout << FF.y << endl;
+      FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa))*HH/aa;    // Inextensibility constraint 
+      // cout << FF.x << endl;
+      // cout << FF.y << endl;     
+
+      if (flag_kappa)
+      {
+        *add_kappasqr=2.*(1.-dot(uk,ukm1));
+        // cout << kappasqr << endl;
+      }
+      *add_FF = FF;
+      *add_SS = (kp+1)*bkm1;
+      break;
   }
 
-  else
-  {
-    getub(&bkm2, &ukm2, kp-2, X);
-    getub(&bkm1, &ukm1, kp-1, X);
-    getub(&bk, &uk, kp, X);
-    getub(&bkp1, &ukp1, kp+1, X);
-    FF = (     (uk+ukm2)/bkm1 - (ukm1+ukp1)/bk
-        + (uk/bk)*( dot(uk,ukm1) + dot(uk,ukp1) )
-        - (ukm1/bkm1)*( dot(ukm1,ukm2) + dot(ukm1,uk) )
-        );
-    FF = FF*(AA/aa);
-    // cout << bkm1 <<endl;
-    // cout << FF.y << endl;
-    FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa))*HH/aa;    // Inextensibility constraint 
-    // cout << FF.x << endl;
-    // cout << FF.y << endl;     
 
-    if (flag_kappa)
-    {
-      *add_kappasqr=2.*(1.-dot(uk,ukm1));
-      // cout << kappasqr << endl;
-    }
-    *add_FF = FF;
-  }
-  *add_SS = (kp+1)*bkm1;
-}
+  // if (kp == 0)
+  // {
+  //   dX = X[kp-1+1]-Xzero;
+  //   bkm1 = norm(dX);
+  //   ukm1=dX/bkm1;
+  //   getub(&bk, &uk, kp, X);
+  //   getub(&bkp1, &ukp1, kp+1, X);
+  //   FF = (     (uk)/bkm1 - (ukm1+ukp1)/bk
+  //        + (uk/bk)*( dot(uk,ukm1) + dot(uk,ukp1) )
+  //        - (ukm1/bkm1)*( dot(ukm1,uk) )
+  //        );
+  //   FF = FF*AA/aa;
+  //   // Add an extra term for inextensibility constraint
+  //   FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa))*HH/aa; 
+  //   // cout << FF.z << endl;
+  //   *add_kappasqr=0.;
+  //   *add_FF = FF;
+  // }
+
+  // else if (kp == 1)
+  // {
+  //   dX = X[kp-2+1]-Xzero;
+  //   bkm2 = norm(dX);
+  //   ukm2 = dX/bkm2;
+  //   getub(&bkm1, &ukm1, kp-1, X);
+  //   getub(&bk, &uk, kp, X);
+  //   getub(&bkp1, &ukp1, kp+1, X);
+  //   FF = (     (uk+ukm2)/bkm1 - (ukm1+ukp1)/bk
+  //       + (uk/bk)*( dot(uk,ukm1) + dot(uk,ukp1) )
+  //       - (ukm1/bkm1)*( dot(ukm1,ukm2) + dot(ukm1,uk) )
+  //       );
+  //   FF = FF*(AA/aa);
+  //   // cout << FF.z << endl;
+  //   FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa) )*HH/aa;   // Inextensibility constraint
+  //   *add_kappasqr=0.;
+  //   *add_FF = FF;
+  // }
+
+
+  // else if (kp == Np-2)
+  // {
+  //   getub(&bkm2, &ukm2, kp-2, X);
+  //   getub(&bkm1, &ukm1, kp-1, X);
+  //   getub(&bk, &uk, kp, X);
+
+  //   FF = (     (uk+ukm2)/bkm1 - (ukm1)/bk
+  //       + (uk/bk)*( dot(uk,ukm1))
+  //       - (ukm1/bkm1)*( dot(ukm1,ukm2) + dot(ukm1,uk) )
+  //       );    
+  //   FF = FF*(AA/aa);
+  //   // cout << bk << endl;
+  //   // cout << FF.z << endl;
+  //   FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa))*HH/aa;    // Inextensibility constraint 
+  //   // cout << FF.z << endl;
+  //   *add_kappasqr=0.;
+  //   *add_FF = FF;
+  // }
+
+  // else if (kp == Np-1)
+  // {
+  //   getub(&bkm2, &ukm2, kp-2, X);
+  //   getub(&bkm1, &ukm1, kp-1, X);
+  
+  //   FF = (     (ukm2)/bkm1
+  //       - (ukm1/bkm1)*( dot(ukm1,ukm2) )
+  //       );
+  //   FF = FF*(AA/aa);
+  //   // cout << bkm1 << endl;
+  //   FF = FF - (ukm1*(bkm1-aa))*HH/aa;
+  //   *add_kappasqr=0.;
+  //   *add_FF = FF;
+  // }
+
+  // else
+  // {
+  //   getub(&bkm2, &ukm2, kp-2, X);
+  //   getub(&bkm1, &ukm1, kp-1, X);
+  //   getub(&bk, &uk, kp, X);
+  //   getub(&bkp1, &ukp1, kp+1, X);
+  //   FF = (     (uk+ukm2)/bkm1 - (ukm1+ukp1)/bk
+  //       + (uk/bk)*( dot(uk,ukm1) + dot(uk,ukp1) )
+  //       - (ukm1/bkm1)*( dot(ukm1,ukm2) + dot(ukm1,uk) )
+  //       );
+  //   FF = FF*(AA/aa);
+  //   // cout << bkm1 <<endl;
+  //   // cout << FF.y << endl;
+  //   FF = FF - (ukm1*(bkm1-aa) - uk*(bk-aa))*HH/aa;    // Inextensibility constraint 
+  //   // cout << FF.x << endl;
+  //   // cout << FF.y << endl;     
+
+  //   if (flag_kappa)
+  //   {
+  //     *add_kappasqr=2.*(1.-dot(uk,ukm1));
+  //     // cout << kappasqr << endl;
+  //   }
+  //   *add_FF = FF;
+  // }
+  // *add_SS = (kp+1)*bkm1;
+ }
 /**************************/
-void diagnos(int p){
+// void diagnos(int p){
   // cout << time<<'\t' ;
   // for (int ip=0;ip<Np;ip++){
   //   cout << y[3*ip] <<'\t' << y[3*ip+1]  <<'\t' << y[3*ip+2] << '\t' ;
@@ -178,20 +280,40 @@ void diagnos(int p){
   //   }
 
   // outfile.close(); 
-  cout << p << endl;
+  // cout << p << endl;
   // cout << time << '\t' << y[0] << endl;
-}
+// }
 /**************************/
 void iniconf(double y[]){
   vec3 R[Np];	// R is the position of the beads.
-  double k = 0.5;      // determines the frequency for initial configuration
+  double k = 1;      // determines the frequency for initial configuration
+  double CurvLength = 0;  // determines the total length of the curve
   for (int ip=0;ip<Np;ip++){
     R[ip].x=0.;
     R[ip].z=aa*double(ip+1);
-    R[ip].y=aa*sin(2*M_PI*k*aa*double(ip+1));
+    // R[ip].y=aa*sin(M_PI*k*aa*double(ip+1));
+    R[ip].y = 0;    
+    if (ip>0)
+    {
+        CurvLength = CurvLength + sqrt((R[ip].x - R[ip-1].x)*(R[ip].x - R[ip-1].x) + (R[ip].y - R[ip-1].y)*(R[ip].y - R[ip-1].y) + (R[ip].z - R[ip-1].z)*(R[ip].z - R[ip-1].z));
+        // cout << CurvLength << endl;
+    }
+    else
+    {
+        CurvLength = CurvLength + sqrt((R[ip].x)*(R[ip].x)+(R[ip].y)*(R[ip].y)+(R[ip].z)*(R[ip].z));
+    }
     // cout << M_PI << endl;
     y[3*ip]=R[ip].x;
     y[3*ip+1]=R[ip].y;
     y[3*ip+2]=R[ip].z;
   }
+
+  // R[Np-1].y = aa;
+  // y[3*Np-2] = aa;
+  // cout << aa << endl;
+  // for (int ip = 0; ip < Np; ++ip)
+  // {
+  //     R[ip].y = R[ip].y/CurvLength;
+  // }
+
 }
