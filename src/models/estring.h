b984900a0b8ed4b9ec74a2d68a721d0cf2f9b6a0
Vipin Agrawal <vipinagrawal25@gmail.com>
2018-07-29 10:06:00 +0530

    Changed the value of HH back to the formulae described by Wada and Netz

--- src/models/estring.h
+++ src/models/estring.h
@@ -9,7 +9,7 @@ unsigned int const pdim=3*Np;
 double const AA = 0.001;		  		// AA is bending rigidity constant (AA = EI), unit -> Pa.m^4
 double const aa =  1.0/(double)(Np); 	// distance between two nodes.
                                         //which should be constant accodrding to the constraint.
-double const HH = AA/(aa*aa);		// H should be atleast 1500 times of A. Follow: bit.ly/2r23lmA unit -> Pa.m^2
+double const HH = 16*AA/(aa*aa);		// H should be atleast 1500 times of A. Follow: bit.ly/2r23lmA unit -> Pa.m^2
 // double const HH = 0.;
 double const Gamma=1.;
 double const Z0=0.;
