4d923d282fbf835869d76d41a52c551b37d5061b
Vipin Agrawal <vipinagrawal25@gmail.com>
2018-07-29 10:06:49 +0530

    Few minor changes

--- src/models/harmonic.cpp
+++ src/models/harmonic.cpp
@@ -6,13 +6,19 @@ using namespace std;
 /**************************/
 void eval_rhs(double time,double y[],double rhs[]){
   //  double y[ndim],rhs[ndim];
-  rhs[0]=y[1];
-  rhs[1]=-omega2*y[0] ;
+  // rhs[0]=y[1];
+  // rhs[1]=-y[0] ;
+  
+	rhs[0] = y[1];
+	rhs[1] = -y[0]+9.81;
+
+  // rhs[0]=-(2+cos(0.1*time))*(2+cos(0.1*time))*y[1];;
+  // rhs[1]=y[0];
   // + f0*sin(fom*time);
   //  cout<<f0*sin(fom*time)<<time<<"\n";
 }
 /**************************/
 void iniconf(double y[]){
-  y[0]=1.;
-  y[1]=0;
+  y[0]=0.;
+  y[1]=0.;
 }
