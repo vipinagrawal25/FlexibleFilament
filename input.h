a1e404817b66d60eab5af48db70f675429093130
Vipin Agrawal <vipinagrawal25@gmail.com>
2018-09-18 11:16:18 +0200

    Introduced tdiag to save files after a particular interval

--- input.h
+++ input.h
@@ -3,7 +3,8 @@
 /* ----------------------------------------*/
 unsigned int const Nensemble=1;
 double dt = pow(10,-3);
-double TMAX=5000;
+double TMAX=10;
 int idiag=100;
+int tdiag=1;	/*This decides after how much time interval the file should be saved.*/
 /* ----------------------------------------*/
 #endif /* !FILE_INPUT_SEEN */
