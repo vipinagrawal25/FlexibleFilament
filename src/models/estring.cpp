515f8b8e424273f0d219be8b595f26a09be30c7c
Vipin Agrawal <vipinagrawal25@gmail.com>
2018-09-24 14:58:31 +0200

    Implemented new optimized way of Stokes RP tensor

--- src/models/estring.cpp
+++ src/models/estring.cpp
@@ -2,20 +2,15 @@
 #include<fstream>
 #include "ode.h"
 #include "modules/3vec.h"
+#include "modules/2Tens.h"
 #include "model.h"
-#include <cmath>
+// #include <cmath>
 using namespace std;
 
-/**************************/
-void dHdR(int kp, vec3 X[], vec3* add_FF, double* add_kappasqr, double* add_SS, bool flag_kappa);
-void getub(double *bk, vec3 *uk, int kp, vec3 X[]);
-int MatrixtoVector(int i, int j, int N);
-void GetRij(vec3 X[], int i, int j, double *Distance, vec3 *rij);
-/**************************/
 void eval_rhs(double time,double y[],double rhs[], bool flag_kappa, double CurvSqr[], double SS[]){
   vec3 R[Np],dR[Np], EForce[Np], EForce_ip;  // R is the position of the beads.
   // double CurvSqr[Np];
-  double kappasqr, MaterialCoordinate, Mobility[Np*(Np+1)/2][6], c1, c2;
+  double kappasqr, MaterialCoordinate;
 
   // Initializing Mobility Matrix.
   for (int i = 0; i < Np*(Np+1)/2; ++i)
@@ -49,11 +44,11 @@ void eval_rhs(double time,double y[],double rhs[], bool flag_kappa, double CurvS
   
   // cout << EForce[Np-1].x << endl;
 
-  if (UseRP == 'Y')
+  if (UseRP == 'Yold')
   {
     // cout << "Heheheheh" ;
     vec3 rij;
-    double distance;
+    double distance, Mobility[Np*(Np+1)/2][6], c1, c2;
     for (int ip = 0; ip < Np; ++ip)
     {
       for (int jp = ip; jp < Np; ++jp)
@@ -69,17 +64,13 @@ void eval_rhs(double time,double y[],double rhs[], bool flag_kappa, double CurvS
             GetRij(R, ip, jp, &distance, &rij);
             c1 = 1.0/(8*M_PI*viscosity*distance)*(1+ dd*dd*1.0/(6*distance*distance));
             c2 = 1.0/(8*M_PI*viscosity*distance*distance*distance)*(1-dd*dd*1.0/(2*distance*distance));
-            // cout << c1 << '\t' << c2 << '\t' << distance << '\t' << ip << '\t' << jp << '\t' << rij.x << '\t' << rij.y << '\t' << rij.z <<endl;
             Mobility[MatrixtoVector(ip,jp,Np)][0] = c1+c2*(rij.x)*(rij.x);
             Mobility[MatrixtoVector(ip,jp,Np)][1] = c2*(rij.x)*(rij.y);
             Mobility[MatrixtoVector(ip,jp,Np)][2] = c2*(rij.x)*(rij.z);
             
-            // Mobility[MatrixtoVector(ip,jp,Np)][3] = 1.0/(8*M_PI*viscosity*distance)*(1+ dd*dd*1.0/(24*distance*distance)+(rij.y)*(rij.x)*(1-dd*dd*1.0/(8*distance*distance)));
             Mobility[MatrixtoVector(ip,jp,Np)][3] = c1+c2*(rij.y)*(rij.y);
             Mobility[MatrixtoVector(ip,jp,Np)][4] = c2*(rij.y)*(rij.z);
 
-            // Mobility[MatrixtoVector(ip,jp,Np)][6] = 1.0/(8*M_PI*viscosity*distance)*(1+ dd*dd*1.0/(24*distance*distance)+(rij.x)*(rij.)*(1-dd*dd*1.0/(8*distance*distance)));
-            // Mobility[MatrixtoVector(ip,jp,Np)][7] = 1.0/(8*M_PI*viscosity*distance)*(1+ dd*dd*1.0/(24*distance*distance)+(rij.x)*(rij.z)*(1-dd*dd*1.0/(8*distance*distance)));
             Mobility[MatrixtoVector(ip,jp,Np)][5] = c1+c2*(rij.z)*(rij.z);
         }
       }
@@ -87,7 +78,7 @@ void eval_rhs(double time,double y[],double rhs[], bool flag_kappa, double CurvS
 
     for (int ip = 0; ip < Np; ++ip)
     {
-      vec3 dR_temp(0.,0.,0.);
+      vec3 dR_temp();
       for (int jp = 0; jp < Np; ++jp)
       {
         dR_temp.x = dR_temp.x + (Mobility[MatrixtoVector(ip,jp,Np)][0])*(EForce[jp].x)
@@ -111,6 +102,37 @@ void eval_rhs(double time,double y[],double rhs[], bool flag_kappa, double CurvS
     }
 
   } 
+
+  else if (UseRP == 'Y')
+  {
+    // mu_ij represents the one element of mobility matrix (Size: NXN). 
+    // Every element of the matrix itself is a 2nd rank tensor and the dimension of that should 3x3.
+
+    Tens2 mu_ij, mu_ii;
+    double d_rij;
+    vec3 rij;
+
+    mu_ii = dab/(3*M_PI*viscosity*dd)
+    // rij = R[j]-R[i] and d_rij is just the norm of this value.
+
+    for (int ip = 0; ip < Np; ++ip)
+    {
+        // The mu_ij in the next line represents the mobility tensor when j is equal to i and in 
+        // response to that the for loop is started from ip+1 .
+
+        dR[ip] = dR[ip] + dot(mu_ii,EForce[ip]);
+        
+        for (int jp = ip+1; jp < Np; ++jp)
+        {
+            GetRij(R, ip, jp, &d_rij, &rij);
+            double c1 = 1/(8*M_PI*viscosity*d_rij);
+            mu_ij = c1*(dab + rij*rij/(d_rij*d_rij) + dd*dd/(2*d_rij*d_rij)*(dab/3 - rij*rij/(d_rij*d_rij)));
+            dR[ip] = dR[ip] + dot(mu_ij,EForce[jp]);
+            dR[jp] = dR[jp] + dot(mu_ij,EForce[jp]);
+        }
+    }
+  }
+
   else
   {
     for (int ip = 0; ip < Np; ++ip)
@@ -143,7 +165,8 @@ void dHdR(int kp, vec3 X[], vec3* add_FF, double* add_kappasqr, double* add_SS, 
      // This function calculates the force at every node which is a function of X, time.
   vec3 ukm2(0.,0.,0.), ukm1(0.,0.,0.), uk(0.,0.,0.), ukp1(0.,0.,0.), Xzero(0.,0.,0.), dX(0.,0.,0.);
   double bkm2, bkm1, bk, bkp1;
-  vec3 FF = *add_FF;              // Since I am passing the address of force in add_FF and the same goes for Kapppsqr
+  vec3 FF = *add_FF;             
+    // Since I am passing the address of force in add_FF and the same goes for Kapppsqr
 
   //vec3 FF;
   Xzero.x=0.; Xzero.y=0.;Xzero.z=Z0;
@@ -293,7 +316,8 @@ void iniconf(double y[]){
     // R[ip].y = 0;    
     if (ip>0)
     {
-        CurvLength = CurvLength + sqrt((R[ip].x - R[ip-1].x)*(R[ip].x - R[ip-1].x) + (R[ip].y - R[ip-1].y)*(R[ip].y - R[ip-1].y) + (R[ip].z - R[ip-1].z)*(R[ip].z - R[ip-1].z));
+        CurvLength = CurvLength + sqrt((R[ip].x - R[ip-1].x)*(R[ip].x - R[ip-1].x)
+            + (R[ip].y - R[ip-1].y)*(R[ip].y - R[ip-1].y) + (R[ip].z - R[ip-1].z)*(R[ip].z - R[ip-1].z));
         // cout << CurvLength << endl;
     }
     else
@@ -337,7 +361,11 @@ int MatrixtoVector(int i, int j, int N)
 void GetRij(vec3 R[], int i, int j, double *Distance, vec3 *rij)
 {
   /*This calculated the distance at two index i and j on the elastic string*/
-  // *Distance = (R[i].x - R[j].x)*(R[i].x - R[j].x) + (R[i].y - R[j].y)*(R[i].y - R[j].y) + (R[i].z - R[j].z)*(R[i].z - R[j].z);
+  // *Distance = (R[i].x - R[j].x)*(R[i].x - R[j].x) + (R[i].y - R[j].y)*(R[i].y - R[j].y) 
+    // + (R[i].z - R[j].z)*(R[i].z - R[j].z);
+    // The reason behind to calculate distance in this function is so that the code would waste time 
+    // only once calculating the norm not every time the function norm is to be called.
+    
   *rij = R[j] - R[i];
   double Dis = norm(R[j]-R[i]); 
   *Distance = Dis;
