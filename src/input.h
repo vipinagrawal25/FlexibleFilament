e8945b1d83505882e8194a52c8aebda537c17fbe
Vipin Agrawal <vipinagrawal25@gmail.com>
2018-10-25 14:44:13 +0200

    Smaller things

--- src/input.h
+++ src/input.h
@@ -1,10 +1,1 @@
-#ifndef FILE_INPUT_SEEN
-#define FILE_INPUT_SEEN
-/* ----------------------------------------*/
-unsigned int const Nensemble=1;
-double dt = pow(10,-3);
-double TMAX=10;
-int idiag=100;
-int tdiag=1;	/*This decides after how much time interval the file should be saved.*/
-/* ----------------------------------------*/
-#endif /* !FILE_INPUT_SEEN */
+../input.h
