4f323d2e48e9e6af2ecb2e3dbcff94e9f318a774
Vipin Agrawal <vipinagrawal25@gmail.com>
2019-01-01 15:48:08 +0100

    Made codes more user friendly

--- PythonScripts/CurvaturePlot.py
+++ PythonScripts/CurvaturePlot.py
@@ -5,11 +5,11 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # This section is intended for calculating the surface plot of curvature.
 Np = 20
-dd = loadtxt('../output/curvature.txt')
-dd_SS = loadtxt('../output/material_point.txt')
+dd = loadtxt('10/curvature.txt')
+dd_SS = loadtxt('10/material_point.txt')
 dd = dd
-Y = dd_SS
-timeAxis = dd[:,0]
+Y = dd_SS[1500,2000,:]
+timeAxis = 0.02*dd[1500:2000,0]
 
 
 IndexAxis = arange(Np)
@@ -25,5 +25,5 @@ surf = ax.contourf(X,Y,heightAxis)
 cbar = colorbar(surf)
 ax.set_xlabel('time')
 ax.set_ylabel('material_point')
-ax.set_zlabel('Curvature')
+# ax.set_zlabel('Curvature')
 show()
--- PythonScripts/moviewriter.py
+++ PythonScripts/moviewriter.py
@@ -22,28 +22,31 @@ fig = plt.figure()
 ax1=fig.add_subplot(1,1,1,projection='3d')
 # ax2=fig.add_subplot(1,2,2)
 
+FILE = 'output'
+height = 1
+
 def MakePlot3D(ax,Xaxis,Yaxis,Zaxis,isnap):
 	# ax = fig.add_subplot(2,1,1, projection='3d')
 	ax.plot(Xaxis,Yaxis,Zaxis,'o-')
 	ax.set_xlim(-1, 1)
 	ax.set_ylim(-0.5, 0.5)
-	ax.set_zlim(0,1.3)
+	ax.set_zlim(0,height)
 	plt.title(str(time[isnap]))
 
-def MakePlot2D(ax,Xaxis,Yaxis,isnap):
-	# ax.subplot(2,1,2)
-	ax.plot(Xaxis,Yaxis,'o-')
-	ax.set_xlim(-1,1.3)
-	ax.set_ylim(-1,1.3)
-	plt.title(str(time[isnap]))
-	# plt.hold(False)
-	ax.grid(True)
+# def MakePlot2D(ax,Xaxis,Yaxis,isnap):
+# 	# ax.subplot(2,1,2)
+# 	ax.plot(Xaxis,Yaxis,'o-')
+# 	ax.set_xlim(-1,1.3)
+# 	ax.set_ylim(-1,1.3)
+# 	plt.title(str(time[isnap]))
+# 	# plt.hold(False)
+# 	ax.grid(True)
 
-time = loadtxt('output/time.txt')
+time = loadtxt(FILE+'/time.txt')
 
-with writer.saving(fig,"output/movie.mp4", 100):
+with writer.saving(fig,FILE+"/movie.mp4", 100):
 	for isnap in range(1,itn,1):
-		dd = loadtxt('output/position'+str(isnap)+'.txt')
+		dd = loadtxt(FILE+'/position'+str(isnap)+'.txt')
 		xx = dd[:,0]
 		yy = dd[:,1]
 		zz = dd[:,2]
@@ -54,10 +57,10 @@ with writer.saving(fig,"output/movie.mp4", 100):
 		writer.grab_frame()
 		print('plot = ' + str(isnap)+ 'Done')
 
-# with writer.saving(fig, "output/movie.mp4", 100):	
+# with writer.saving(fig, FILE+"/movie.mp4", 100):	
 #     for isnap in range(1,itn,1):
 #     	# start = time.time()
-#     	dd = loadtxt('output/position'+str(isnap)+'.txt')
+#     	dd = loadtxt(FILE+'/position'+str(isnap)+'.txt')
 #     	zz = dd[:,2]
 #     	yy = dd[:,1]
 #     	xx = dd[:,0]
