7b22e9b5d09822fafd5556d0bc35f0277af52947
Vipin Agrawal <vipinagrawal25@gmail.com>
2019-01-23 10:31:13 +0100

    Small changes in tdiag and other things

--- input.h
+++ input.h
@@ -2,9 +2,9 @@
 #define FILE_INPUT_SEEN
 /* ----------------------------------------*/
 unsigned int const Nensemble=1;
-double dt = pow(10,-3);
-double TMAX=10;
-int idiag=100;
-int tdiag=1;	/*This decides after how much time interval the file should be saved.*/
+double dt = pow(10,-4);
+double TMAX=100;
+int idiag=10;
+double tdiag=1;	/*This decides after how much time interval the file should be saved.*/
 /* ----------------------------------------*/
 #endif /* !FILE_INPUT_SEEN */
