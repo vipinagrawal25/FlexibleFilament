72000a840ecfb32db6aa58269ca95d5f0dc07e7e
Dhrubaditya Mitra <dhruba@nordita.org>
2019-01-29 11:43:14 +0100

    Minor changes: included the possibilty of different compilers in different machines.

--- input.h
+++ input.h
@@ -3,7 +3,7 @@
 /* ----------------------------------------*/
 unsigned int const Nensemble=1;
 double dt = pow(10,-4);
-double TMAX=100;
+double TMAX=10;
 int idiag=10;
 double tdiag=1;	/*This decides after how much time interval the file should be saved.*/
 /* ----------------------------------------*/
