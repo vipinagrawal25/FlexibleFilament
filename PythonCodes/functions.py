bc17c8c30fb54004bd9a3b618a1ba0de0a16a5ab
Vipin Agrawal <vipinagrawal25@gmail.com>
2019-02-21 15:39:26 +0100

    Made function file and script file for python plots

---
+++ PythonCodes/functions.py
@@ -0,0 +1,34 @@
+from pylab import *
+import ipywidgets as wid
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+def curvatureplot( FILE='output', Np=100, omega=3 ):
+	dd = loadtxt(FILE+'/curvature.txt')
+	dd_SS = loadtxt(FILE+'/material_point.txt')
+	dd = dd
+	Y = dd_SS
+	timeAxis = loadtxt(FILE+'/time.txt')*omega/3.14
+	IndexAxis = arange(Np)
+	heightAxis = dd[:,1:Np+1]
+	# heightAxis = ndimage.rotate(heightAxis,90)
+	# imshow(heightAxis,cmap=plt.cm.gray)
+	# colorbar()
+
+	(IndexAxis,X) = meshgrid(IndexAxis, timeAxis)
+	fig = figure()
+	ax = fig.add_subplot(111)
+	surf = ax.contourf(X,Y,heightAxis)
+	cbar = colorbar(surf)
+	ax.set_xlabel('time')
+	ax.set_ylabel('material_point')
+	# ax.set_zlabel('Curvature')
+	# ax.set_zlabel('Curvature')
+	show()
+	return 
+
+def MSD_plot(FILE='.',step=1):
+	MSD = loadtxt(FILE+'/MSD.txt',delimiter=',',usecols=range(2000))
+	plt.plot(MSD[1::step])
+
+
---
+++ PythonCodes/script.py
@@ -0,0 +1,7 @@
+from functions import *
+
+frequency = 3;
+Np = 100;
+
+curvatureplot(omega=frequency)
+MSD_plot()
