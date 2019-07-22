11778b7e9372cf5ebac46b9730ecbd2f164e4d93
Dhrubaditya Mitra <dhruba.mitra@gmail.com>
2019-07-22 18:59:45 +0200

    brought in the header files for vectors and second rank tensors.

---
+++ gpu_prelim/2Tens.h
@@ -0,0 +1,166 @@
+#ifndef FILE_2Tens_SEEN
+#define FILE_2Tens_SEEN
+/*---------------------------------------*/
+#include <iostream>
+#include<math.h>
+#include "3vec.h"
+using namespace std;
+/*-------------------------*/
+class Tens2{
+public:
+  double xx,xy,xz,yx,yy,yz,zx,zy,zz;
+  Tens2();
+  Tens2(int,int,int,int,int,int,int,int,int);
+  Tens2(float,float,float,float,float,float,float,float,float);
+  Tens2(double,double,double,double,double,double,double,double,double);
+    // definining operators 
+  Tens2 inline operator+(Tens2);
+  Tens2 inline operator-(Tens2);
+  Tens2 inline operator*(double);
+  Tens2 inline operator/(double);
+
+private:
+
+};
+
+Tens2::Tens2(){
+  xx = 0.0;
+  xy = 0.0;
+  xz = 0.0;
+  
+  yx = 0.0;
+  yy = 0.0;
+  yz = 0.0;
+  
+  zx = 0.0;
+  zy = 0.0;
+  zz = 0.0;
+}
+
+Tens2::Tens2(int a, int b, int c, int d, int e, int f, int g, int h, int i){
+  xx = (double) a;
+  xy = (double) b;
+  xz = (double) c;
+  
+  yx = (double) d;
+  yy = (double) e;
+  yz = (double) f;
+  
+  zx = (double) g;
+  zy = (double) h;
+  zz = (double) i;
+}
+
+Tens2::Tens2(float a, float b, float c, float d, float e, float f, float g, float h, float i){
+  xx = (double) a;
+  xy = (double) b;
+  xz = (double) c;
+  
+  yx = (double) d;
+  yy = (double) e;
+  yz = (double) f;
+  
+  zx = (double) g;
+  zy = (double) h;
+  zz = (double) i;
+}
+
+Tens2::Tens2(double a, double b, double c, double d, double e, double f, double g, double h, double i){
+  xx =  a;
+  xy =  b;
+  xz =  c;
+
+  yx =  d;
+  yy =  e;
+  yz =  f;
+  
+  zx =  g;
+  zy =  h;
+  zz =  i;
+}
+
+Tens2 Tens2::operator+(Tens2 param){
+  Tens2 temp;
+  temp.xx = xx+param.xx;
+  temp.xy = xy+param.xy;
+  temp.xz = xz+param.xz;
+
+  temp.yx = yx+param.yx;
+  temp.yy = yy+param.yy;
+  temp.yz = yz+param.yz;
+  
+  temp.zx = zx+param.zx;
+  temp.zy = zy+param.zy;
+  temp.zz = zz+param.zz;
+
+  return(temp);
+}
+
+Tens2 Tens2::operator-(Tens2 param){
+  Tens2 temp;
+  temp.xx = xx-param.xx;
+  temp.xy = xy-param.xy;
+  temp.xz = xz-param.xz;
+
+  temp.yx = yx-param.yx;
+  temp.yy = yy-param.yy;
+  temp.yz = yz-param.yz;
+  
+  temp.zx = zx-param.zx;
+  temp.zy = zy-param.zy;
+  temp.zz = zz-param.zz;
+
+  return(temp);
+}
+
+Tens2 Tens2::operator*(double param){
+  Tens2 temp;
+  temp.xx=param*xx;
+  temp.xy=param*xy;
+  temp.xz=param*xz;
+
+  temp.yx=param*yx;
+  temp.yy=param*yy;
+  temp.yz=param*yz;
+
+  temp.zx=param*zx;
+  temp.zy=param*zy;
+  temp.zz=param*zz;
+
+  return(temp);
+}
+
+Tens2 operator*(const double param, Tens2 a)
+{
+    // This function is just here because multiplication between double and a Tensor needs to be commutative.
+    return(a*param);
+}
+
+Tens2 Tens2::operator/(double param){
+  Tens2 temp;
+  temp.xx=xx/param;
+  temp.xy=xy/param;
+  temp.xz=xz/param;
+
+  temp.yx=yx/param;
+  temp.yy=yy/param;
+  temp.yz=yz/param;
+
+  temp.zx=zx/param;
+  temp.zy=zy/param;
+  temp.zz=zz/param;
+
+  return(temp);
+}
+
+void PTens2(Tens2 a){
+  cout<<a.xx<<"\t"<<a.xy<<"\t"<<a.xz<<"\n"<<a.yx<<"\t"<<a.yy<<"\t"<<a.yz<<"\t"<<a.zx<<"\t"<<a.zy<<"\t"<<a.zz<<"\n";
+}
+
+
+// This defines the kronecker delta function or a unit tensor matrix. dab = \delta_{\alpha \beta}
+Tens2 dab(1.,0.,0.,0.,1.,0.,0.,0.,1.);
+
+/*---------------------------------------*/
+#endif /* !FILE_3vec_SEEN */
+
---
+++ gpu_prelim/3vec.h
@@ -0,0 +1,156 @@
+#ifndef FILE_3vec_SEEN
+#define FILE_3vec_SEEN
+/*---------------------------------------*/
+#include <iostream>
+#include<math.h>
+#include "2Tens.h"
+using namespace std;
+/*-------------------------*/
+class vec3{
+public:
+  double x,y,z;
+  vec3();
+  vec3(int,int,int);
+  vec3(float,float,float);
+  vec3(double,double,double);
+    // definining operators 
+  vec3 operator+(vec3);
+  vec3 operator-(vec3);
+  vec3 operator*(double);
+  vec3 operator/(double);
+  Tens2 operator*(vec3);
+
+private:
+};
+
+vec3::vec3(){
+  x = 0.0;
+  y = 0.0;
+  z = 0.0;
+}
+vec3::vec3(int a, int b, int c){
+  x = (double) a;
+  y = (double) b;
+  z = (double) c;
+}
+vec3::vec3(float a, float b, float c){
+  x = (double) a;
+  y = (double) b;
+  z = (double) c;
+}
+vec3::vec3(double a, double b, double c){
+  x =  a;
+  y =  b;
+  z =  c;
+}
+
+vec3 vec3::operator+(vec3 param){
+  vec3 temp;
+  temp.x = x+param.x;
+  temp.y = y+param.y;
+  temp.z = z+param.z;
+  return(temp);
+}
+vec3 vec3::operator-(vec3 param){
+  vec3 temp;
+  temp.x = x-param.x;
+  temp.y = y-param.y;
+  temp.z = z-param.z;
+  return(temp);
+}
+
+vec3 vec3::operator*(double param){
+  vec3 temp;
+  temp.x=param*x;
+  temp.y=param*y;
+  temp.z=param*z;
+  return(temp);
+}
+
+Tens2 vec3::operator*(vec3 param)
+{   
+    // This is a direct product of 2 vectors. Assuming vec3 a column vector with 3 entries
+    // then this operator defines w = uv' (Where v' is the transpose of the matrix.)
+    // It should be noted that this product is not commutative.
+
+    Tens2 temp;
+    temp.xx = x*(param.x);
+    temp.xy = x*(param.y);
+    temp.xz = x*(param.z);
+
+    temp.yx = y*(param.x);
+    temp.yy = y*(param.y);
+    temp.yz = y*(param.z);
+
+    temp.zx = z*(param.x);
+    temp.zy = z*(param.y);
+    temp.zz = z*(param.z);
+
+    return(temp);
+}
+
+vec3 vec3::operator/(double param){
+  vec3 temp;
+  temp.x=x/param;
+  temp.y=y/param;
+  temp.z=z/param;
+  return(temp);
+}
+
+double dot(vec3 a, vec3 b){
+  double temp;
+  temp = a.x*b.x+a.y*b.y+a.z*b.z;
+  return(temp);
+}
+vec3 cross(vec3 a, vec3 b){
+  vec3 temp;
+  temp.x = a.y*b.z-a.z*b.y;
+  temp.y = -(a.x*b.z-b.x*a.z);
+  temp.z = a.x*b.y-a.y*b.x;
+  return(temp);
+}
+double norm(vec3 a){
+  return( sqrt( a.x*a.x+a.y*a.y+a.z*a.z) );}
+/*---------------------------------------*/
+double sqnorm(vec3 a){
+  return( a.x*a.x+a.y*a.y+a.z*a.z) ;}
+/*---------------------------------------*/
+void PVec3(vec3 a){
+  cout<<a.x<<"\t"<<a.y<<"\t"<<a.z<<"\n";
+}
+
+vec3 dot(Tens2 a, vec3 b)
+{
+    // Ci = Aij*Bj; (Einstein Convention)
+    // It should be a simple matrix multiplication, where A is 3X3 matrix and B is 3X1.
+
+    vec3 temp;
+
+    temp.x = (a.xx)*(b.x)+(a.xy)*(b.y)+(a.xz)*(b.z);
+    temp.y = (a.yx)*(b.x)+(a.yy)*(b.y)+(a.yz)*(b.z);
+    temp.z = (a.zx)*(b.x)+(a.zy)*(b.y)+(a.zz)*(b.z);
+
+    return (temp);
+}
+
+// Tens2 DirectProduct(vec3 a, vec3 b)
+// {
+//     Tens2 temp;
+//     temp.xx = a.x*b.x;
+//     temp.xy = a.x*b.y;
+//     temp.xz = a.x*b.z;
+    
+//     temp.yx = a.y*b.x;
+//     temp.yy = a.y*b.y;
+//     temp.yz = a.y*b.z;
+
+//     temp.zx = a.z*b.x;
+//     temp.zy = a.z*b.y;
+//     temp.zz = a.z*b.z;
+
+//     return(temp);
+// }
+
+/*---------------------------------------*/
+#endif /* !FILE_3vec_SEEN */
+
